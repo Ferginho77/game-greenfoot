f5645e2548980198b45f7e74ac96e6c2c5214d27
Ferginho77 <134527352+Ferginho77@users.noreply.github.com>
2026-01-07 09:43:09 +0700

    Edit Block
    Added game timer and win condition logic.

--- MyWorld.java
+++ MyWorld.java
@@ -1,4 +1,5 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;  
+import java.util.List;// (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class MyWorld here.
@@ -9,26 +10,61 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class MyWorld extends World
 {
 
+    
+    private int timer = 600;
+    private int lavaEventCount = 0;
+    private final int MAX_EVENT = 2;
+    private boolean gameOver = false;
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        // Sesuaikan ukuran dunia dengan ukuran gambar kamu
     super(960, 540, 1); 
-    
-    // Membuat objek gambar GreenfootImage
     GreenfootImage bg = new GreenfootImage("lava.jpg");
-    
-    // Opsional: Memaksa gambar agar ukurannya pas dengan ukuran World
     bg.scale(getWidth(), getHeight());
-    
-    // Memasang gambar ke background
     setBackground(bg);
     prepare();
     }
+    
+    public void act(){
+        timer --;
+    
+        showText("Time : " + (timer / 60), 400, 50);
+
+        if(timer <= 0){
+            deactivateRandomBlocks();
+             lavaEventCount++;
+
+            if(lavaEventCount >= MAX_EVENT){
+                playerWin();
+            } else {
+                timer = 600; // lanjut ke ronde berikutnya
+            }
+        }
+
+        }
+        
+        private void playerWin(){
+        gameOver = true;
+        showText("YOU WIN!", getWidth()/2, getHeight()/2);
+        Greenfoot.stop();
+    }
+    
+
+
+private void deactivateRandomBlocks(){
+    List<FalseBlock> blocks = getObjects(FalseBlock.class);
+
+    for(FalseBlock b : blocks){
+        if(Greenfoot.getRandomNumber(2) == 0){
+            b.setActive(false);
+        }
+    }
+}
+
+    
     
     public void prepare(){
     Player player = new Player();
@@ -48,6 +84,24 @@ public class MyWorld extends World
     addObject(new litblock(), 374, 303);
     addObject(new litblock(), 594, 303);
     addObject(new litblock(), 794, 303);
+    
+    
+    
+    // Block bawah
+    addObject(new Block(),48,493);
+    addObject(new FalseBlock(), 194, 443);
+    addObject(new FalseBlock(), 294, 393);
+    addObject(new FalseBlock(), 394, 443);
+    addObject(new FalseBlock(), 494, 393);
+    addObject(new FalseBlock(), 594, 443);
+    addObject(new FalseBlock(), 694, 393);
+    addObject(new FalseBlock(), 794, 443);
+
+    // Block atas
+    addObject(new FalseBlock(), 198, 303);
+    addObject(new FalseBlock(), 374, 303);
+    addObject(new FalseBlock(), 594, 303);
+    addObject(new FalseBlock(), 794, 303);
     
     //Ground Lava
     addObject(new Ground(), 548, 592);
