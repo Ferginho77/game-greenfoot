5dd721d72eb3fc64eda2a9e9bdb6dde2115dde1f
Ferginho77 <134527352+Ferginho77@users.noreply.github.com>
2026-01-14 10:37:32 +0700

    Refactor MyWorld class and update game logic

--- MyWorld.java
+++ MyWorld.java
@@ -1,109 +1,70 @@
-import greenfoot.*;  
-import java.util.List;// (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
+import java.util.List;
+import java.util.Collections;
 
-/**
- * Write a description of class MyWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class MyWorld extends World
-{
+public class MyWorld extends World {
+    private int timer = 300; 
+    private int ronde = 1;
+    private boolean eventTriggered = false;
 
-    
-    private int timer = 600;
-    private int lavaEventCount = 0;
-    private final int MAX_EVENT = 2;
-    private boolean gameOver = false;
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
-    public MyWorld()
-    {    
-    super(960, 540, 1); 
-    GreenfootImage bg = new GreenfootImage("lava.jpg");
-    bg.scale(getWidth(), getHeight());
-    setBackground(bg);
-    prepare();
+    public MyWorld() {    
+        super(960, 540, 1);
+        prepare();
     }
-    
-    public void act(){
-        timer --;
-    
-        showText("Time : " + (timer / 60), 400, 50);
 
-        if(timer <= 0){
-            deactivateRandomBlocks();
-             lavaEventCount++;
+    public void act() {
+        if (ronde <= 2) {
+            runRound();
+        } else {
+            showText("SELAMAT! ANDA MENANG!", getWidth()/2, getHeight()/2);
+            Greenfoot.stop();
+        }
+    }
 
-            if(lavaEventCount >= MAX_EVENT){
-                playerWin();
+    private void runRound() {
+        if (timer > 0) {
+            timer--;
+            showText("Ronde: " + ronde, 100, 80);
+            showText("Waktu: " + (timer/60), getWidth()/2, 50);
+        } else if (!eventTriggered) {
+            deactivateTwoBlocks();
+            eventTriggered = true;
+            timer = -100;
+        }
+
+        if (eventTriggered && timer < -90) {
+            if (ronde < 2) {
+                ronde++;
+                timer = 300; // Reset waktu untuk ronde 2
+                eventTriggered = false;
             } else {
-                timer = 600; // lanjut ke ronde berikutnya
+                ronde = 3; 
             }
         }
+    }
 
-        }
+    private void deactivateTwoBlocks() {
+        List<FalseBlock> blocks = getObjects(FalseBlock.class);
+        // Mengacak urutan list agar blok yang diambil acak
+        Collections.shuffle(blocks);
         
-        private void playerWin(){
-        gameOver = true;
-        showText("YOU WIN!", getWidth()/2, getHeight()/2);
-        Greenfoot.stop();
-    }
-    
-
-
-private void deactivateRandomBlocks(){
-    List<FalseBlock> blocks = getObjects(FalseBlock.class);
-
-    for(FalseBlock b : blocks){
-        if(Greenfoot.getRandomNumber(2) == 0){
-            b.setActive(false);
+        int count = 0;
+        for (FalseBlock b : blocks) {
+            if (count < 4) { 
+                b.setAsTrap();
+                count++;
+            }
         }
     }
-}
 
-    
-    
-    public void prepare(){
-    Player player = new Player();
-    addObject(player, 40, 415);
-    
-    //Block Bawah
-    addObject(new Block(),48,493);
-    addObject(new litblock(), 194, 443);
-    addObject(new litblock(), 294, 393);
-    addObject(new litblock(), 394, 443);
-    addObject(new litblock(), 494, 393);
-    addObject(new litblock(), 594, 443);
-    addObject(new litblock(), 694, 393);
-    addObject(new litblock(), 794, 443);
-    //Block Atas
-    addObject(new litblock(), 198, 303);
-    addObject(new litblock(), 374, 303);
-    addObject(new litblock(), 594, 303);
-    addObject(new litblock(), 794, 303);
-    
-    
-    
-    // Block bawah
-    addObject(new Block(),48,493);
-    addObject(new FalseBlock(), 194, 443);
-    addObject(new FalseBlock(), 294, 393);
-    addObject(new FalseBlock(), 394, 443);
-    addObject(new FalseBlock(), 494, 393);
-    addObject(new FalseBlock(), 594, 443);
-    addObject(new FalseBlock(), 694, 393);
-    addObject(new FalseBlock(), 794, 443);
+    private void prepare() {
+        Player player = new Player();
+        addObject(player, 50, 350);
+        addObject(new Block(), 50, 493);
 
-    // Block atas
-    addObject(new FalseBlock(), 198, 303);
-    addObject(new FalseBlock(), 374, 303);
-    addObject(new FalseBlock(), 594, 303);
-    addObject(new FalseBlock(), 794, 303);
-    
-    //Ground Lava
-    addObject(new Ground(), 548, 592);
+        for (int i = 0; i < 11; i++) {
+            addObject(new FalseBlock(), 150 + (i * 75), 400);
+        }
+        addObject(new Ground(), 548, 590);
     }
 }
